6b3e6a723d26011e8b738f96a28f68bf4c6f8815
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-02-22 16:26:28 +0530

    [Pragya] add. Use Services Layer to get Simple Greeting message using input

--- greetingapp/src/main/java/com/greetingapp/controller/GreetingController.java
+++ greetingapp/src/main/java/com/greetingapp/controller/GreetingController.java
@@ -39,4 +39,14 @@ public class GreetingController {
     public Greeting getGreetings() {
         return new Greeting(greetingService.getGreetingMessage());
     }
+
+    //UC3
+
+    // GET Request - Returns a greeting message with optional name inputs
+    @GetMapping("/greetinput")
+    public Greeting getGreeting(@RequestParam(required = false) String firstname,
+                                @RequestParam(required = false) String lastname) {
+        return new Greeting(greetingService.getGreetingMessage(firstname, lastname));
+    }
+    //http://localhost:8080/api/greetinput?firstname=Pragya&lastname=Sable
 }
